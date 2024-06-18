6ee7df967b5d9fbb7752196483ea28be9899671a
akhil <akhilhhh94@gmail.com>
2024-06-18 19:26:08 +0530

    my commin

---
+++ app.js
@@ -0,0 +1,21 @@
+$(function() {
+    $('#hreo-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed: 1000,
+        responsive:{
+            0:{
+                nav: false,
+            },
+            600:{
+           
+            },
+            1000:{
+        
+            }
+        }
+    })
+});
