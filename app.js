b880930b410479314730e4e264fa5c7fe2145cb5
akhil <akhilhhh94@gmail.com>
2024-06-20 10:06:23 +0530

    another example

--- app.js
+++ app.js
@@ -1,21 +1,2 @@
-$(function() {
-    $('#hreo-slider').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        items:1,
-        dots:false,
-        smartSpeed: 1000,
-        responsive:{
-            0:{
-                nav: false,
-            },
-            600:{
-           
-            },
-            1000:{
-        
-            }
-        }
-    })
-});
+var scene = document.getElementById('schenes');
+var parallaxInstance = new Parallax(scene);
